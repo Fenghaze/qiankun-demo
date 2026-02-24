6c9ae49b3321b20c1ef303efcd9e1edc3c8c010b
fenghaze <feng_haze@163.com>
2026-02-24 18:57:41 +0800

    设置样式

--- base-web/src/App.vue
+++ base-web/src/App.vue
@@ -14,15 +14,21 @@
       @click="changeTheme(1)"
       >黑色主题</el-button
     >
+    <el-button @click="changeFontSize(fontSize + 1)">增大字体</el-button>
+    <el-button @click="changeFontSize(fontSize - 1)">减小字体</el-button>
+    <div class="test-rem">
+      <p>这是一个测试rem单位的文本，当前字体大小：{{ fontSize }}px</p>
+    </div>
   </div>
   <!-- qiankun子应用挂载DOM -->
   <div id="sub-container"></div>
 </template>
 
 <script setup>
-import { computed, onMounted } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { userInfoStore } from "./stores/userInfo";
 const userInfoData = userInfoStore();
+const fontSize = ref(18);
 const curTheme = computed({
   get() {
     return userInfoData.user.theme;
@@ -37,5 +43,20 @@ function initApp() {
 function changeTheme(themeVal) {
   curTheme.value = themeVal;
 }
+function changeFontSize(val) {
+  const root = document.documentElement;
+  root.style.setProperty(`--font-size`, `${val}px`);
+  fontSize.value = val;
+}
 initApp();
 </script>
+
+<style>
+* {
+  font-size: var(--font-size);
+}
+.test-rem {
+  width: 10rem;
+  height: 10rem;
+}
+</style>
--- vue-webpack/src/App.vue
+++ vue-webpack/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <h1>子应用1：vue+webpack打包的项目</h1>
-    <router-link to="/vue-webpack/home">首页</router-link>
+    <router-link to="/vue-webpack/home">虚拟滚动</router-link>
     |
-    <router-link to="/vue-webpack/about">关于</router-link>
+    <router-link to="/vue-webpack/about">骨架屏</router-link>
     <router-view></router-view>
   </div>
 </template>
